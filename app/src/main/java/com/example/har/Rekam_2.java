0b6d26255d6bd799181f945b7e25a24329bc9ce3
Ksatria Bhuana <ksatriabhuana@helmproyek.local>
2020-09-06 01:47:30 +0700

    Scroll View

---
+++ app/src/main/java/com/example/har/Rekam_2.java
@@ -0,0 +1,14 @@
+package com.example.har;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rekam_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rekam_2);
+    }
+}
